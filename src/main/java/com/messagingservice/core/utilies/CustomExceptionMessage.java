5836f3a3679000a197047bfb650abbacc80f4db8
Oguzhan Sali <salioguzhan.61@gmail.com>
2025-07-17 10:35:09 +0300

    Fixed grammar in exception messages and improve consistency.

--- src/main/java/com/messagingservice/core/utilies/CustomExceptionMessage.java
+++ src/main/java/com/messagingservice/core/utilies/CustomExceptionMessage.java
@@ -6,10 +6,10 @@ public class CustomExceptionMessage {
     public static final String INVALID_CREDENTIALS= "Invalid credentials";
     public static final String BLOCKER_USER_NOT_FOUND= "Blocker user not found";
     public static final String USER_TO_BE_BLOCKED_NOT_FOUND = "User to be blocked not found";
-    public static final String MESSAGE_NOT_FOUND = "No Messages to delete found";
+    public static final String MESSAGE_NOT_FOUND = "No messages found to delete";
     public static final String SENDER_NOT_FOUND = "Sender not found";
     public static final String RECEIVER_NOT_FOUND = "Receiver not found";
-    public static final String HAVE_BEEN_BLOCKED ="You are blocked by this user";
-    public static final String USER_ALREADY_BLOCKED = "User Already Blocked";
-    public static final String USER_CANNOT_BLOCK_THEMSELF="User cannot block themself";
+    public static final String HAVE_BEEN_BLOCKED ="You have been blocked by this user";
+    public static final String USER_ALREADY_BLOCKED = "User is already blocked";
+    public static final String USER_CANNOT_BLOCK_THEMSELF="A user cannot block themself";
 }
