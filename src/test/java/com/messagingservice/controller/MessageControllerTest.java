2181656ec0f11bc2ba95580db0a9dad727920cfc
Oguzhan Sali <salioguzhan.61@gmail.com>
2025-07-17 12:33:28 +0300

    Updated test URLs to reflect recent changes in MessageController endpoint paths.

--- src/test/java/com/messagingservice/controller/MessageControllerTest.java
+++ src/test/java/com/messagingservice/controller/MessageControllerTest.java
@@ -55,67 +55,57 @@ public class MessageControllerTest {
     }
 
     @Test
-    void sendMessage_shouldReturnSavedMessage() throws Exception {
+    void sendMessageShouldReturnSavedMessage() throws Exception {
         MessageRequestDTO requestDTO = new MessageRequestDTO("senderUser", "receiverUser", "Hello there!");
 
         when(messageService.sendMessage(any())).thenReturn(sampleMessage);
 
-        mockMvc.perform(post("/api/messages/send")
+        mockMvc.perform(post("/api/messages")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestDTO)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.senderUsername").value("senderUser"))
-                .andExpect(jsonPath("$.receiverUsername").value("receiverUser"))
-                .andExpect(jsonPath("$.content").value("Hello there!"))
+                .andExpect(jsonPath("$.message").value("Message sent successfully"))
+                .andExpect(jsonPath("$.createdMessage.senderUsername").value("senderUser"))
+                .andExpect(jsonPath("$.createdMessage.receiverUsername").value("receiverUser"))
+                .andExpect(jsonPath("$.createdMessage.content").value("Hello there!"))
                 .andDo(print());
     }
 
     @Test
-    void getMessagesBySender_shouldReturnMessageList() throws Exception {
+    void getMessagesBySenderShouldReturnMessageList() throws Exception {
         List<Message> messages = Arrays.asList(sampleMessage);
         when(messageService.getMessagesBySender("senderUser")).thenReturn(messages);
 
-        mockMvc.perform(get("/api/messages/senderUser/sender"))
+        mockMvc.perform(get("/api/messages/sender/senderUser"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].senderUsername").value("senderUser"))
                 .andDo(print());
     }
 
     @Test
-    void getMessagesByReceiver_shouldReturnMessageList() throws Exception {
+    void getMessagesByReceiverShouldReturnMessageList() throws Exception {
         List<Message> messages = Arrays.asList(sampleMessage);
         when(messageService.getMessagesByReceiver("receiverUser")).thenReturn(messages);
 
-        mockMvc.perform(get("/api/messages/receiverUser/receiver"))
+        mockMvc.perform(get("/api/messages/receiver/receiverUser"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].receiverUsername").value("receiverUser"))
                 .andDo(print());
     }
 
     @Test
-    void getAllMessages_shouldReturnAllMessages() throws Exception {
-        List<Message> messages = Arrays.asList(sampleMessage);
-        when(messageService.getAllMessages()).thenReturn(messages);
-
-        mockMvc.perform(get("/api/messages/all"))
+    void deleteMessageShouldReturnSuccessMessage() throws Exception {
+        mockMvc.perform(delete("/api/messages/1"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].messageId").value("1"))
+                .andExpect(jsonPath("$.message").value("Message deleted successfully"))
                 .andDo(print());
     }
 
     @Test
-    void deleteMessage_shouldReturnSuccessMessage() throws Exception {
-        mockMvc.perform(delete("/api/messages/1/delete"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(""))
-                .andDo(print());
-    }
-
-    @Test
-    void deleteMessage_whenNotFound_shouldReturn404() throws Exception {
+    void deleteMessageWhenNotFoundShouldReturn404() throws Exception {
         Mockito.doThrow(new MessageNotFoundException("Message not found")).when(messageService).deleteMessageById("999");
 
-        mockMvc.perform(delete("/api/messages/999/delete"))
+        mockMvc.perform(delete("/api/messages/999"))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("Message not found"))
                 .andDo(print());
